faf7aa8742fc18b6c01a7d858ec23cd56900f896
Igor Kalashnikov <igor.kalashnikov@me.com>
2024-01-04 04:01:41 +0400

    homebrew setup script

---
+++ brew.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# replacing outdated macos bundled tools with newer versions
+brew install bash coreutils grep git curl wget
+
+# neovim and dependencies
+brew install neovim ripgrep fd
+
+# nodejs
+brew install n
+
+# shell
+brew install fish fzf starship
+
+# useful tools
+brew install httpie colordiff htop
+
+# terminal font
+brew install font-fira-code-nerd-font --cask
