393fa96cf60081741ca2aaf2314ea4fd49b35677
Igor Kalashnikov <igor.kalashnikov@me.com>
2024-01-05 18:00:50 +0400

    <leader>bd to remove buffer

--- nvim/lua/silent/plugins/bufferline.lua
+++ nvim/lua/silent/plugins/bufferline.lua
@@ -9,6 +9,7 @@ return {
 	keys = {
 		{ "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
 		{ "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+		{ "<leader>bd", "<cmd>bdelete<cr>", desc = "Delete buffer" },
 	},
 	config = function(_, opts)
 		require("bufferline").setup({
