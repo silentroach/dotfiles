6b9ac3b51cfa3a3a5bda964911e59d0e2a99982a
Igor Kalashnikov <igor.kalashnikov@me.com>
2024-01-06 04:41:31 +0400

    + telescope for nvim

---
+++ nvim/lua/silent/plugins/telescope.lua
@@ -0,0 +1,33 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+		},
+	},
+	config = function()
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "File live grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+		vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "Find recently opened files" })
+
+		local telescope = require("telescope")
+
+		telescope.setup({
+			defaults = {
+				file_ignore_patterns = {
+					"node_modules",
+				},
+			},
+		})
+
+		telescope.load_extension("fzf")
+	end,
+}
