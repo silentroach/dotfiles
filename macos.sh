fdf43feaecb5bc064f0c82333364fde5c26d8811
Igor Kalashnikov <igor.kalashnikov@me.com>
2024-01-14 23:18:26 +0400

    macos config script

---
+++ macos.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Set sidebar icon size to Small
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
+
+# Reducing repeat delays
+defaults write -g InitialKeyRepeat -int 15
+defaults write -g KeyRepeat -int 2
