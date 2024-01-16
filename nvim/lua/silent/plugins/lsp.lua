c00a33cb9820a0478d0981464daad0cfd1988077
Igor Kalashnikov <igor.kalashnikov@me.com>
2024-01-16 23:41:52 +0400

    rename keybinding

--- nvim/lua/silent/plugins/lsp.lua
+++ nvim/lua/silent/plugins/lsp.lua
@@ -36,6 +36,7 @@ return {
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {})
 		end,
 	},
 	{
