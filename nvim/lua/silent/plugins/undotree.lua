146eaeaded128fa9b8730f25651fa15779adb6eb
Igor Kalashnikov <igor.kalashnikov@me.com>
2024-01-16 17:00:06 +0400

    undotree settings

--- nvim/lua/silent/plugins/undotree.lua
+++ nvim/lua/silent/plugins/undotree.lua
@@ -3,4 +3,13 @@ return {
 	keys = {
 		{ "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Undotree Toggle" },
 	},
+	config = function()
+		vim.g.undotree_HighlightChangedWithSign = 0
+		vim.g.undotree_SetFocusWhenToggle = 1
+		vim.g.undotree_ShortIndicators = 1
+		vim.g.undotree_HelpLine = 0
+
+		vim.g.undotree_WindowLayout = 2
+		vim.g.undotree_DiffpanelHeight = 7
+	end,
 }
