78f8bb832d9b05324bb67c2cf26524bbad4f125d
Igor Kalashnikov <igor.kalashnikov@me.com>
2024-02-10 19:17:15 +0400

    more macos defaults

--- macos.sh
+++ macos.sh
@@ -9,3 +9,13 @@ defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
 # Reducing repeat delays
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
+
+# Menubar clock widget
+defaults write com.apple.menuextra.clock "IsAnalog" -bool TRUE
+defaults write com.apple.menuextra.clock "ShowDayOfWeek" -bool FALSE
+
+# Dock
+defaults write com.apple.dock "tilesize" -int "36"
+
+# Do not show recent apps in dock
+defaults write com.apple.dock show-recents -bool FALSE
