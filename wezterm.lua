6187c66b131a5520dcd13957da6c1f99496387ed
Igor Kalashnikov <igor.kalashnikov@me.com>
2024-02-29 00:31:57 +0400

    better wezterm settings

--- wezterm.lua
+++ wezterm.lua
@@ -12,18 +12,20 @@ config.font = wezterm.font({
 	family = "MonoLisa Nerd Font",
 	weight = "Light",
 })
-config.font_size = 13
-config.line_height = 1.05
+config.font_size = 14
+config.line_height = 1.1
 config.cell_width = 0.9
 
 config.audible_bell = "Disabled"
 
 config.window_padding = {
-	left = 20,
-	right = 14,
-	top = 10,
-	bottom = 10,
+	left = 2,
+	right = 2,
+	top = 0,
+	bottom = 0,
 }
+
+config.hide_tab_bar_if_only_one_tab = true
 
 config.window_frame = {
 	font = wezterm.font({
