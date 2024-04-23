e21cec76c374d57a2db126f682a0c68671dada1b
Igor Kalashnikov <igor.kalashnikov@me.com>
2024-04-24 02:02:09 +0400

    wezterm fix

--- wezterm.lua
+++ wezterm.lua
@@ -9,22 +9,30 @@ config.color_scheme = "Catppuccin Frappe"
 config.max_fps = 120
 
 config.font = wezterm.font({
-	family = "MonoLisa Nerd Font",
+	family = "MonoLisa Nerd Font Mono",
 	weight = "Light",
 })
-config.font_size = 13
+config.font_size = 12
 config.line_height = 1.1
 config.cell_width = 0.9
 
 config.audible_bell = "Disabled"
 
 config.use_resize_increments = true
+-- config.use_resize_increments = false
 config.window_padding = {
 	left = 2,
 	right = 2,
 	top = 0,
 	bottom = 0,
 }
+-- config.window_padding = {
+-- 	left = 20,
+-- 	right = 0,
+-- 	top = 0,
+-- 	bottom = 10,
+-- }
+
 config.window_frame = {
 	font = wezterm.font({
 		family = "Lucida Grande",
@@ -32,7 +40,7 @@ config.window_frame = {
 	font_size = 12,
 }
 
-config.initial_rows = 50
+config.initial_rows = 40
 config.initial_cols = 160
 
 config.hide_tab_bar_if_only_one_tab = true
