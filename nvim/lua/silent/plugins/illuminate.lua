e62ca64ca9c841043af7406481125b7f1f5b04c3
Igor Kalashnikov <igor.kalashnikov@me.com>
2024-04-25 01:14:28 +0400

    navigate with [[ and ]] over illuminated in nvim

--- nvim/lua/silent/plugins/illuminate.lua
+++ nvim/lua/silent/plugins/illuminate.lua
@@ -5,5 +5,14 @@ return {
 	opts = { delay = 200 },
 	config = function(_, opts)
 		require("illuminate").configure(opts)
+
+		local function map(key, dir, buffer)
+			vim.keymap.set("n", key, function()
+				require("illuminate")["goto_" .. dir .. "_reference"](false)
+			end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+		end
+
+		map("]]", "next")
+		map("[[", "prev")
 	end,
 }
