db39d85b49e37b6cfc7200f2aa95208625e0b739
Igor Kalashnikov <igor.kalashnikov@me.com>
2024-04-25 01:22:35 +0400

    mini.comment

--- nvim/lua/silent/plugins/comment.lua
+++ nvim/lua/silent/plugins/comment.lua
@@ -1,5 +1,12 @@
 return {
-	-- comment/uncomment hotkeys gc/gcc
-	"numToStr/Comment.nvim",
-	config = true,
+	"echasnovski/mini.comment",
+	dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+	keys = { { "gc", mode = { "n", "v" } } },
+	opts = {
+		hooks = {
+			pre = function()
+				require("ts_context_commentstring.internal").update_commentstring({})
+			end,
+		},
+	},
 }
